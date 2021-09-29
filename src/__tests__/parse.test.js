d3ff063aebc0e8dc5363c1dba89e5216d7b9a80c
Igor Kozlovsky <igor.lemon.kozlovsky@gmail.com>
2021-09-29 21:49:33 +0300

    Add @scope param

--- src/__tests__/parse.test.js
+++ src/__tests__/parse.test.js
@@ -13,14 +13,52 @@ describe('Test Parser', () => {
 
   it("Should return package name and version for not correct lerna tag values", () => {
     expect(parse('some-package@')).toBe(undefined)
-    expect(parse('1.0.1@1.1.1')).toBe(undefined)
     expect(parse('some-package@some-package')).toBe(undefined)
     expect(parse('some-package')).toBe(undefined)
     expect(parse('1.0.1')).toBe(undefined)
   })
 
   it("Should return package name and version for correct lerna tag", () => {
-    const result = parse('some-package@1.0.1')
-    expect(result).toMatchObject({packageName: 'some-package', version: '1.0.1'})
+    expect(parse('some-package@2.0.1')).toMatchObject({
+      scope: '',
+      packageName: 'some-package',
+      version: '2.0.1'
+    })
+
+    expect(parse('1.0.1@1.1.1')).toMatchObject({
+      scope: '',
+      packageName: '1.0.1',
+      version: '1.1.1'
+    })
+
+    expect(parse('@1-2-3/1.0.1@1.1.1')).toMatchObject({
+      scope: '@1-2-3',
+      packageName: '1.0.1',
+      version: '1.1.1'
+    })
+
+    expect(parse('@scope/test-test@1.0.1')).toMatchObject({
+      scope: '@scope',
+      packageName: 'test-test',
+      version: '1.0.1'
+    })
+
+    expect(parse('@scope-test/test-test@1.1.1')).toMatchObject({
+      scope: '@scope-test',
+      packageName: 'test-test',
+      version: '1.1.1'
+    })
+
+    expect(parse('@scope-test2/test2-test@0.0.1')).toMatchObject({
+      scope: '@scope-test2',
+      packageName: 'test2-test',
+      version: '0.0.1'
+    })
+
+    expect(parse('@scope_test~1.test_dash/test2-test.pr~1_1@0.0.1')).toMatchObject({
+      scope: '@scope_test~1.test_dash',
+      packageName: 'test2-test.pr~1_1',
+      version: '0.0.1'
+    })
   })
 })
